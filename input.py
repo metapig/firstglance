7f15861c3a9652e3dc8bf4709f0923041a6e9088
metapig <2696764827@qq.com>
2023-05-30 23:52:06 +0800

    modified:   input.py

--- input.py
+++ input.py
@@ -1,24 +1,26 @@
 welcome_words = "Hello there, it's a investigation."
-print(welcome_words+"\n")
+print(welcome_words)
 
 dic = {}
 arr = []
 flag = True
 while flag:
-    resp = input("Would you like to response?(yes/no)")
+    resp = input("\nWould you like to response? (yes/no) ")
 
     if resp == 'no':
         break
     elif resp != 'yes': 
         continue
     else:
-        name = input("Could you kindly tell me your name?")
-        age = input("and your age?")
+        name = input("\nCould you kindly tell me your name? ")
+        age = input("\nand your age? ")
         age = int(age)
         answer = []
 
         while answer != 'quit':
-            answer = input('Then tell me the place you wanna go if possible(enter "quit" to quit)')
+            prompt = '\nThen tell me the place you wanna go if possible (enter "quit" to quit)'
+            prompt += '\n'+"you can enter for multiple times "
+            answer = input(prompt)
             if answer != 'quit':
                 arr.append(answer)
 
@@ -27,7 +29,9 @@ while flag:
             "place": arr
         }
 for name, info in dic.items():
-    print("\n")
-    print(f"{name.title().lstrip()}'s info:")
+ 
+    print(f"\n{name.title().lstrip()}'s info:")
     for name_info,name_list in info.items():
-        print(f"{name_info.title()}:{name_list}")
+        print(f"\t{name_info.title()}:{name_list}")
+
+input("Press ang key to quit")
